3f9f3fa94dd046706f3093d791f190bc8466567d
Joe Darling <joe.darling@bt.com>
2014-03-18 09:14:05 +0000

    Initial router connection method

--- lib/router_runner.rb
+++ lib/router_runner.rb
@@ -5,5 +5,13 @@ require_relative 'offlicence'
 require_relative 'offlicence_router'
 
 module RouterRunner
-  # Your code goes here...
+  def self.with_connection_details(connection_details)
+    connection = RouterConnection.new(connection_details)
+    connection.build.connect do |connected_connection|
+      yield connected_connection
+    end
+  end
 end
+
+
+
---
+++ spec/router_runner_spec.rb
@@ -0,0 +1,35 @@
+require "spec_helper"
+
+describe RouterConnection, "#connect" do
+  let(:router_connection)    { double(:router_connection, build: built_connection) }
+  let(:built_connection)     { double(:built_connection, connect: connected_connection) }
+  let(:connected_connection) { double }
+  let(:connection_details) { {router_username: "fred", router_password: "secret", router_hostname: "router1", jump_server_username: "bill", jump_server_password: "real_secret", jump_server_ip: "1.2.3.4"} }
+
+  before do
+    RouterConnection.stub(:new).and_return(router_connection)
+  end
+
+  it "runs the command on the router" do
+    RouterConnection.should_receive(:new).with(connection_details)
+    RouterRunner.with_connection_details(connection_details)
+  end
+
+  it "builds the connection" do
+    router_connection.should_receive(:build)
+    RouterRunner.with_connection_details(connection_details)
+  end
+
+  it "connects the connection" do
+    built_connection.should_receive(:connect)
+    RouterRunner.with_connection_details(connection_details)
+  end
+  
+  it "should run the command" do
+    built_connection.stub(:connect).and_yield connected_connection
+    connected_connection.should_receive(:run).with("some command")
+    RouterRunner.with_connection_details(connection_details) do |c|
+      c.run("some command")
+    end
+  end
+end
