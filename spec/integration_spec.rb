81ac789642ec5ead226eaca6e1c7ebcc0cb76d8e
Joe Darling <joe.darling@bt.com>
2014-03-18 10:17:51 +0000

    fix integration test

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,6 +1,12 @@
 require "spec_helper"
 
 describe "running a command on a router" do
+  let(:telnet_connection) { double(:telnet_connection, puts: "stuff", waitfor: true) }
+
+  before do
+    Net::SSH::Telnet.stub(:new).and_return(telnet_connection)
+  end
+
   it "should send the command to the router" do
     connection_details = {router_username: "fred", 
                           router_password: "secret", 
@@ -10,7 +16,7 @@ describe "running a command on a router" do
                           jump_server_ip: "1.2.3.4"}
     result = ""
     RouterRunner.with_connection_details(connection_details) do | router |
-      result = connected_router.run("some command")
+      result = router.run("some command")
     end
     expect(result).to eq("Some stuff from a router")
   end
